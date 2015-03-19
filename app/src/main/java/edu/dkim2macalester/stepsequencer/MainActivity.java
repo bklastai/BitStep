1cc9916847ef3f04f05ca6ccdc58b1666375fbda
bklastai <benas.klastaitis@gmail.com>
2015-03-19 18:35:46 -0500

    fixed grid item selection

--- app/src/main/java/edu/dkim2macalester/stepsequencer/MainActivity.java
+++ app/src/main/java/edu/dkim2macalester/stepsequencer/MainActivity.java
@@ -1,11 +1,9 @@
 package edu.dkim2macalester.stepsequencer;
 
-<<<<<<< HEAD
-import android.content.Context;
-=======
+
 import android.media.AudioManager;
 import android.media.SoundPool;
->>>>>>> origin/master
+
 import android.os.Bundle;
 
 import android.content.pm.ActivityInfo;
@@ -13,29 +11,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.GridView;
-<<<<<<< HEAD
+
 import android.widget.Toast;
-=======
-import android.widget.ListAdapter;
 
 import java.util.ArrayList;
->>>>>>> origin/master
+
 
 public class MainActivity extends ActionBarActivity {
 
     private GridView gridView;
-<<<<<<< HEAD
+
     private int numberOfGrids = 1; //change this value when adding new gridviews (to the right of the screen)
     private boolean[] selection = new boolean[numberOfGrids*256];
-=======
-    private SoundPool soundPool;
-    private ArrayList<sound> mSounds = null;
 
->>>>>>> origin/master
+    private SoundPool soundPool;
+    private ArrayList<Sound> mSounds = null;
+
+
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -48,158 +42,122 @@ public class MainActivity extends ActionBarActivity {
 
         //Set up the gridview adapter
         gridView = (GridView) findViewById(R.id.gridview);
-<<<<<<< HEAD
         gridView.setAdapter(new GridItemAdapter(this));
 
-<<<<<<< HEAD
+        soundPool = new SoundPool(16, AudioManager.STREAM_MUSIC, 0);
+        mSounds = new ArrayList<>();
+        loadSounds();
+
         //Set up the OnClickListener; change BackgroundResource and set notes 'selection' accordingly
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 if (!selection[position]) {
                     v.setBackgroundResource(R.drawable.black_square);
-                    selection[position] = true;
-                }
-                else {
+                } else {
                     v.setBackgroundResource(R.drawable.empty_square);
-                    selection[position] = false;
                 }
-=======
-        //set up the binary array
-        binArray = new boolean[256];
-        for (int i = 0; i<binArray.length;i++){
-            binArray[i]=false;
-        }
+                selection[position] = !selection[position];
 
-        //set up the soundPool
-=======
-        gridView.setAdapter(new ButtonAdapter(this));
->>>>>>> parent of bfbcaa4... Sound plays when play button is pressed
-        soundPool = new SoundPool(16, AudioManager.STREAM_MUSIC, 0);
-        mSounds = new ArrayList<>();
-
-        loadSounds();
-
-
-
-        //Set up the gridview logic
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                v.setSelected(!v.isSelected());
-                sound s = mSounds.get((position-position%16)/16);
-                soundPool.play(s.getSoundResourceId(),1,1,1,0,1);
-
-
-
-
-<<<<<<< HEAD
-                binArray[position]=!binArray[position];
->>>>>>> origin/master
-=======
->>>>>>> parent of bfbcaa4... Sound plays when play button is pressed
+                Sound s = mSounds.get((position - position % 16) / 16);
+                soundPool.play(s.getSoundResourceId(), 1, 1, 1, 0, 1);
             }
         });
-
 
     }
 
     private void loadSounds() {
-        sound s = new sound();
+        Sound s = new Sound();
         s.setDescription("Kick");
         s.setSoundResourceId(soundPool.load(this,R.raw.kick,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("clap");
         s.setSoundResourceId(soundPool.load(this,R.raw.clap,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("snare");
         s.setSoundResourceId(soundPool.load(this,R.raw.snare,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("rim");
         s.setSoundResourceId(soundPool.load(this,R.raw.rim,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("cowbell");
         s.setSoundResourceId(soundPool.load(this,R.raw.cowbell,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("hh closed");
         s.setSoundResourceId(soundPool.load(this,R.raw.hhclosed,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("hh open");
         s.setSoundResourceId(soundPool.load(this,R.raw.hhopen,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("conga low");
         s.setSoundResourceId(soundPool.load(this,R.raw.congalo,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("conga med");
         s.setSoundResourceId(soundPool.load(this,R.raw.congamed,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("conga hi");
         s.setSoundResourceId(soundPool.load(this,R.raw.congahi,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("tom lo");
         s.setSoundResourceId(soundPool.load(this,R.raw.tomlo,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("tom med");
         s.setSoundResourceId(soundPool.load(this,R.raw.tommed,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("tom hi");
         s.setSoundResourceId(soundPool.load(this,R.raw.tomhi,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("cymbal 1");
         s.setSoundResourceId(soundPool.load(this,R.raw.cymbal1,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("cymbal 2");
         s.setSoundResourceId(soundPool.load(this,R.raw.cymbal2,1));
         mSounds.add(s);
 
-        s = new sound();
+        s = new Sound();
         s.setDescription("cymbal 3");
         s.setSoundResourceId(soundPool.load(this,R.raw.cymbal3,1));
         mSounds.add(s);
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
-    public void play(View v){
-//        for (int i = 0; i<16; i++){
-//
+
+//    public void play(View v){
+//        for (int i=0;i<selection.length;i++){
+//            if (selection[i]){
+//                Sound s = mSounds.get(15-((i-i%16)/16));
+//                soundPool.play(s.getSoundResourceId(),1,1,1,0,1);
+//            }
 //        }
-        for (int i=0;i<binArray.length;i++){
-            if (binArray[i]){
-                sound s = mSounds.get(15-((i-i%16)/16));
-                soundPool.play(s.getSoundResourceId(),1,1,1,0,1);
-            }
-        }
-    }
->>>>>>> origin/master
+//    }
+
 
     public void playMusic(View v){
         String selectedItems = "";
@@ -211,12 +169,6 @@ public class MainActivity extends ActionBarActivity {
         Toast toast = Toast.makeText(getApplicationContext(), "Grid Items Selected: " + selectedItems, Toast.LENGTH_SHORT);
         toast.show();
     }
-
-=======
-//    public void play(Button v){
-//
-//    }
->>>>>>> parent of bfbcaa4... Sound plays when play button is pressed
 
 
     public void initSelection(){
--- app/src/main/java/edu/dkim2macalester/stepsequencer/sound.java
+++ app/src/main/java/edu/dkim2macalester/stepsequencer/sound.java
@@ -3,9 +3,11 @@ package edu.dkim2macalester.stepsequencer;
 /**
  * Created by aronthomas on 3/18/15.
  */
-public class sound {
+public class Sound {
     private String mDescription = "";
     private int mSoundResourceId = -1;
+
+
     public void setDescription(String description) { mDescription = description; }
     public String getDescription() { return mDescription; }
     public void setSoundResourceId(int id) { mSoundResourceId = id; }
