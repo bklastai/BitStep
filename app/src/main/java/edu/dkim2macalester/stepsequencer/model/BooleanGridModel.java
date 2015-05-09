9e709800dd88bd10afa9f4ebc3e02ca16a6d43c2
M Ulmer <mulmer@macalester.edu>
2015-05-09 07:46:15 -0500

    Deleted dead code.

--- app/src/main/java/edu/dkim2macalester/stepsequencer/model/BooleanGridModel.java
+++ app/src/main/java/edu/dkim2macalester/stepsequencer/model/BooleanGridModel.java
@@ -20,10 +20,6 @@ public final class BooleanGridModel {
         selections = temp;
     }
 
-    public BooleanGridModel(boolean[] temp){//constructs a class with the given boolean array
-        selections = temp;
-    }
-
     //constructs a class with the given boolean
     // array AND selects the grid at the
     // specified position (Song's current BGM is updated
@@ -32,27 +28,8 @@ public final class BooleanGridModel {
         selections = temp;
         selections[position] = !isSelected(position);
     }
-
-
-//    public BooleanGridModel setSelected(int position){
-//        //new model to reflect changes
-//        boolean[] temp = new boolean[width*height];
-//
-//        //setting identical to old BGM model aside from desired change
-//        for (int i = 0; i < temp.length; i++){
-//                temp[i]= this.isSelected(i);
-//        }
-//
-//        //making the change
-//        selections[position] = !this.isSelected(position);
-//
-//        BooleanGridModel newBGM = new BooleanGridModel(temp);
-//        return newBGM;
-//    }
-
+    
     public int getSample(int position) { return (position - position % width) / height; }
-
-    public int getTimestamp(int position) { return position % width; }
 
     public boolean isSelected(int position) {
         return selections[position];
